fca35b41d531799d2f45020c3af20e48cf321d2c
hanbing <beatmight@gmail.com>
2020-05-07 11:48:41 +0800

    add test data prediction

--- movecar/scripts.py
+++ movecar/scripts.py
@@ -5,11 +5,14 @@ import zipfile
 import re
 import json
 import random
+
 from tqdm import tqdm
+
 cwd = os.path.abspath(os.path.dirname(__file__))
 if cwd not in sys.path:
     sys.path.append(cwd)
 
+source_root = os.path.abspath(os.path.join(cwd, ".."))
 
 # from sample import predictor
 from correction import rules
@@ -40,9 +43,10 @@ def extract_car_number_response(contents):
     return qa_pairs
 
 
-
-
-if __name__ == "__main__":
+def predict_log():
+    """
+    处理日志中的车牌号码
+    """
     contents = extract_log()
     qa_pairs = extract_car_number_response(contents)
     all_data = []
@@ -65,3 +69,42 @@ if __name__ == "__main__":
         
     with open(".vscode/result.json", "w") as f:
         json.dump(all_data, f, ensure_ascii=False)
+
+
+def predict_test_data():
+    """
+    处理测试集的数据
+    """
+    test_data_file = os.path.join(source_root, "data/movecar/test/data.txt")
+
+    test_data = []
+    with open(test_data_file, "r") as f:
+        for line in f:
+            line = line.strip()
+            test_data.append(["".join(item.split(" ")) for item in line.split("\t")])
+    
+    all_data = []
+    for pair in tqdm(test_data):
+        result_dict = {}
+        text = pair[0]
+        result_dict["原文"] = text
+        rules_correction = rules(text)
+        result_dict["规则纠正结果"] = rules_correction
+        dnn_result = predictor.predict([i for i in text])
+        result_dict["模型纠正结果"] = "".join(dnn_result[:-1])
+        
+        result = predictor.predict([i for i in rules_correction])
+        result_dict["规则加模型纠正结果"] = "".join(result[:-1])
+
+        result_dict["质检人员标注结果"] = pair[1]
+
+        all_data.append(result_dict)
+        
+    with open(".vscode/test_result.json", "w") as f:
+        json.dump(all_data, f, ensure_ascii=False)        
+    
+
+if __name__ == "__main__":
+    # predict_log()
+    # predict_test_data()
+    predict_test_data()
